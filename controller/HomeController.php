e5d8b765fbaa09936d972895b746896c7e08e620
Huong <admin@Apples-MacBook-Pro.local>
2018-06-30 18:55:32 +0700

    db

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,7 +1,13 @@
 <?php
 require_once "BaseController.php";
+require_once "model/HomeModel.php";
+
 class HomeController extends BaseController{
     function getHome(){
+        $model = new HomeModel;
+        $slide = $model->selectSlide();
+        print_r($slide);
+        die;
         return $this->loadView('home');
     }
 }
---
+++ model/HomeModel.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'DBConnect.php';
+class HomeModel extends DBConnect{
+
+    function selectSlide(){
+        $sql = "SELECT * FROM slide WHERE status=1";
+        return $this->loadMoreRows($sql);
+    }
+}
+
+
+?>
