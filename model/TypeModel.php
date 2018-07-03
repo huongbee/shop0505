1458018d3510faf3b20ca5a2b9286d63cc636ffb
Huong <admin@Apples-MacBook-Pro.local>
2018-07-03 19:27:08 +0700

    select products by type

---
+++ model/TypeModel.php
@@ -0,0 +1,43 @@
+<?php
+require_once "DBConnect.php";
+class TypeModel extends DBConnect{
+    /**
+        SELECT p.*, u2.url
+        FROM products p 
+        INNER JOIN categories c 
+        ON c.id = p.id_type
+        INNER JOIN page_url u 
+        ON u.id = c.id_url
+        INNER JOIN page_url u2 
+        ON u2.id = p.id_url
+        WHERE u.url = "phu-kien"
+     */
+    // function selectProductByType($type){
+    //     $sql = "SELECT *  
+    //             FROM products p 
+    //             INNER JOIN page_url u2 
+    //             ON u2.id = p.id_url
+    //             WHERE p.id_type = (SELECT c.id
+    //                 FROM `categories` c 
+    //                 INNER JOIN page_url u
+    //                 ON u.id = c.id_url
+    //                 WHERE u.url = '$type'
+    //             )";
+    //     return $this->loadMoreRows($sql);
+    // }
+    function selectProductByType($type){
+        $sql = "SELECT *  
+                FROM products p 
+                INNER JOIN page_url u2 
+                ON u2.id = p.id_url
+                WHERE p.id_type = (SELECT c.id
+                    FROM `categories` c 
+                    INNER JOIN page_url u
+                    ON u.id = c.id_url
+                    WHERE u.url = ?)";
+        return $this->loadMoreRows($sql,[$type]);
+    }
+}
+
+
+?>
