b705939a4c7691781f9204dc73b1c9f6fa5dd29c
Huong <admin@Apples-MacBook-Pro.local>
2018-07-10 19:38:11 +0700

    html

--- view/ajax/products.php
+++ view/ajax/products.php
@@ -1,158 +1,64 @@
-<div class="shop-inner">
-    <div class="product-grid-area">
-        <ul class="products-grid">
-            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
-                <div class="product-item">
-                    <div class="item-inner">
-                        <div class="product-thumbnail">
-                            <div class="pr-img-area" style="height:300px">
-                                <a title="iPhone 8 Plus 256GB" href="iphone-8-plus-256gb-3.html">
-                                    <figure>
-                                        <img class="first-img" src="public/source/images/products/iphone-8-plus-256gb.png" alt="html template">
-                                        <img class="hover-img" src="public/source/images/products/iphone-8-plus-256gb.png" alt="html template">
-                                    </figure>
-                                </a>
-                                <button type="button" class="add-to-cart-mt">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span> Add to Cart</span>
-                                </button>
-                            </div>
-
+<div class="product-grid-area">
+    <ul class="products-grid">
+        <?php foreach($data as $p):?>
+        <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+            <div class="product-item">
+                <div class="item-inner">
+                    <div class="product-thumbnail">
+                        <?php if($p->promotion_price !=0):?>
+                        <div class="icon-sale-label sale-left">Sale</div>
+                        <?php endif?>
+                        <?php if($p->new ==1):?>
+                        <div class="icon-new-label new-right">New</div>
+                        <?php endif?>
+                        <div class="pr-img-area" style="height:300px">
+                            <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html">
+                                <figure>
+                                    <img class="first-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
+                                    <img class="hover-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
+                                </figure>
+                            </a>
+                            <button type="button" class="add-to-cart-mt">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span> Add to Cart</span>
+                            </button>
                         </div>
-                        <div class="item-info">
-                            <div class="info-inner">
-                                <div class="item-title">
-                                    <a title="iPhone 8 Plus 256GB" href="iphone-8-plus-256gb-3.html">iPhone 8 Plus 256GB</a>
-                                </div>
-                                <div class="item-content">
 
-                                    <div class="item-price">
-                                        <div class="price-box">
-                                            <p class="special-price">
-                                                <span class="price"> 28,790,000 vnd</span>
-                                            </p>
-                                        </div>
+                    </div>
+                    <div class="item-info">
+                        <div class="info-inner">
+                            <div class="item-title">
+                                <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html">
+                                    <?=$p->name?>
+                                </a>
+                            </div>
+                            <div class="item-content">
+
+                                <div class="item-price">
+                                    <div class="price-box">
+                                        <?php if($p->promotion_price !=0):?>
+                                        <p class="special-price">
+                                            <span class="price">
+                                                <?=number_format($p->promotion_price)?> vnd</span>
+                                        </p>
+                                        <p class="old-price">
+                                            <span class="price">
+                                                <?=number_format($p->price)?> vnd </span>
+                                        </p>
+                                        <?php else:?>
+                                        <p class="special-price">
+                                            <span class="price">
+                                                <?=number_format($p->price)?> vnd</span>
+                                        </p>
+                                        <?php endif?>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </li>
-            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
-                <div class="product-item">
-                    <div class="item-inner">
-                        <div class="product-thumbnail">
-                            <div class="pr-img-area" style="height:300px">
-                                <a title="iPhone 8 256GB" href="iphone-8-256gb-4.html">
-                                    <figure>
-                                        <img class="first-img" src="public/source/images/products/iphone-8-256gb.png" alt="html template">
-                                        <img class="hover-img" src="public/source/images/products/iphone-8-256gb.png" alt="html template">
-                                    </figure>
-                                </a>
-                                <button type="button" class="add-to-cart-mt">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span> Add to Cart</span>
-                                </button>
-                            </div>
-
-                        </div>
-                        <div class="item-info">
-                            <div class="info-inner">
-                                <div class="item-title">
-                                    <a title="iPhone 8 256GB" href="iphone-8-256gb-4.html">iPhone 8 256GB</a>
-                                </div>
-                                <div class="item-content">
-
-                                    <div class="item-price">
-                                        <div class="price-box">
-                                            <p class="special-price">
-                                                <span class="price"> 25,790,000 vnd</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
-                <div class="product-item">
-                    <div class="item-inner">
-                        <div class="product-thumbnail">
-                            <div class="pr-img-area" style="height:300px">
-                                <a title="iPhone 8 Plus 64GB" href="iphone-8-plus-64gb-5.html">
-                                    <figure>
-                                        <img class="first-img" src="public/source/images/products/iphone-8-plus-64gb.png" alt="html template">
-                                        <img class="hover-img" src="public/source/images/products/iphone-8-plus-64gb.png" alt="html template">
-                                    </figure>
-                                </a>
-                                <button type="button" class="add-to-cart-mt">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span> Add to Cart</span>
-                                </button>
-                            </div>
-
-                        </div>
-                        <div class="item-info">
-                            <div class="info-inner">
-                                <div class="item-title">
-                                    <a title="iPhone 8 Plus 64GB" href="iphone-8-plus-64gb-5.html">iPhone 8 Plus 64GB</a>
-                                </div>
-                                <div class="item-content">
-
-                                    <div class="item-price">
-                                        <div class="price-box">
-                                            <p class="special-price">
-                                                <span class="price"> 23,990,000 vnd</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
-                <div class="product-item">
-                    <div class="item-inner">
-                        <div class="product-thumbnail">
-                            <div class="pr-img-area" style="height:300px">
-                                <a title="iPhone 8 64GB" href="iphone-8-64gb-6.html">
-                                    <figure>
-                                        <img class="first-img" src="public/source/images/products/iphone-8-64gb.png" alt="html template">
-                                        <img class="hover-img" src="public/source/images/products/iphone-8-64gb.png" alt="html template">
-                                    </figure>
-                                </a>
-                                <button type="button" class="add-to-cart-mt">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span> Add to Cart</span>
-                                </button>
-                            </div>
-
-                        </div>
-                        <div class="item-info">
-                            <div class="info-inner">
-                                <div class="item-title">
-                                    <a title="iPhone 8 64GB" href="iphone-8-64gb-6.html">iPhone 8 64GB</a>
-                                </div>
-                                <div class="item-content">
-
-                                    <div class="item-price">
-                                        <div class="price-box">
-                                            <p class="special-price">
-                                                <span class="price"> 20,990,000 vnd</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div
+            </div>
+        </li>
+        <?php endforeach?>
+    </ul>
+</div>
--- view/type.view.php
+++ view/type.view.php
@@ -39,72 +39,74 @@
                 </div>
               </div>
             </div>
-            <div class="shop-inner">
-              <div class="page-title">
-                <h2><?=$data['nameType']?></h2>
-              </div>
+            <div>
+              <div class="shop-inner" id="new-product">
+                <div class="page-title">
+                  <h2><?=$data['nameType']?></h2>
+                </div>
 
-              <div class="product-grid-area">
-                <ul class="products-grid">
-                  <?php foreach($data['products'] as $p):?>
-                  <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
-                      <div class="product-item">
-                      <div class="item-inner">
-                        <div class="product-thumbnail">
-                          <?php if($p->promotion_price !=0):?>
-                          <div class="icon-sale-label sale-left">Sale</div>
-                          <?php endif?>
-                          <?php if($p->new ==1):?>
-                          <div class="icon-new-label new-right">New</div>
-                          <?php endif?>
-                          <div class="pr-img-area" style="height:300px">
-                            <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html">
-                              <figure>
-                                <img class="first-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
-                                <img class="hover-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
-                              </figure>
-                            </a>
-                            <button type="button" class="add-to-cart-mt">
-                              <i class="fa fa-shopping-cart"></i>
-                              <span> Add to Cart</span>
-                            </button>
-                          </div>
-
-                        </div>
-                        <div class="item-info">
-                          <div class="info-inner">
-                            <div class="item-title">
-                              <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html"><?=$p->name?></a>
+                <div class="product-grid-area">
+                  <ul class="products-grid">
+                    <?php foreach($data['products'] as $p):?>
+                    <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+                        <div class="product-item">
+                        <div class="item-inner">
+                          <div class="product-thumbnail">
+                            <?php if($p->promotion_price !=0):?>
+                            <div class="icon-sale-label sale-left">Sale</div>
+                            <?php endif?>
+                            <?php if($p->new ==1):?>
+                            <div class="icon-new-label new-right">New</div>
+                            <?php endif?>
+                            <div class="pr-img-area" style="height:300px">
+                              <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html">
+                                <figure>
+                                  <img class="first-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
+                                  <img class="hover-img" src="public/source/images/products/<?=$p->image?>" alt="html template">
+                                </figure>
+                              </a>
+                              <button type="button" class="add-to-cart-mt">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span> Add to Cart</span>
+                              </button>
                             </div>
-                            <div class="item-content">
 
-                              <div class="item-price">
-                                <div class="price-box">
-                                <?php if($p->promotion_price !=0):?>
-                                  <p class="special-price">
-                                    <span class="price"><?=number_format($p->promotion_price)?> vnd</span>
-                                  </p>
-                                  <p class="old-price">
-                                    <span class="price"> <?=number_format($p->price)?> vnd </span>
-                                  </p>
-                                  <?php else:?>
-                                  <p class="special-price">
-                                    <span class="price"> <?=number_format($p->price)?> vnd</span>
-                                  </p>
-                                  <?php endif?>
+                          </div>
+                          <div class="item-info">
+                            <div class="info-inner">
+                              <div class="item-title">
+                                <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html"><?=$p->name?></a>
+                              </div>
+                              <div class="item-content">
+
+                                <div class="item-price">
+                                  <div class="price-box">
+                                  <?php if($p->promotion_price !=0):?>
+                                    <p class="special-price">
+                                      <span class="price"><?=number_format($p->promotion_price)?> vnd</span>
+                                    </p>
+                                    <p class="old-price">
+                                      <span class="price"> <?=number_format($p->price)?> vnd </span>
+                                    </p>
+                                    <?php else:?>
+                                    <p class="special-price">
+                                      <span class="price"> <?=number_format($p->price)?> vnd</span>
+                                    </p>
+                                    <?php endif?>
+                                  </div>
                                 </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                  <?php endforeach?>
-                </ul>
-              </div>
-              <div class="pagination-area ">
-                <?=$data['showPagination']?>
+                    </li>
+                    <?php endforeach?>
+                  </ul>
+                </div>
+                <div class="pagination-area ">
+                  <?=$data['showPagination']?>
+                </div>
               </div>
             </div>
           </div>
@@ -463,7 +465,13 @@
             id: idType // $_GET['id']
           },
           success:function(responseFromPHP){
-            console.log(responseFromPHP)
+            if($('#new-product').hasClass('shop-inner')){
+              $('#new-product').html(responseFromPHP)
+              $('#new-product').removeClass('shop-inner')
+            }
+            else{
+              $('#new-product').prepend(responseFromPHP)
+            }
           },
           error:function(){
             console.log("error!!!")
