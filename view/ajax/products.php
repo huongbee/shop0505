47553269f2bd49a0b929e369eea43f2c5d6acd67
Huong <admin@Apples-MacBook-Pro.local>
2018-07-10 19:06:41 +0700

    products

---
+++ view/ajax/products.php
@@ -0,0 +1,158 @@
+<div class="shop-inner">
+    <div class="product-grid-area">
+        <ul class="products-grid">
+            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+                <div class="product-item">
+                    <div class="item-inner">
+                        <div class="product-thumbnail">
+                            <div class="pr-img-area" style="height:300px">
+                                <a title="iPhone 8 Plus 256GB" href="iphone-8-plus-256gb-3.html">
+                                    <figure>
+                                        <img class="first-img" src="public/source/images/products/iphone-8-plus-256gb.png" alt="html template">
+                                        <img class="hover-img" src="public/source/images/products/iphone-8-plus-256gb.png" alt="html template">
+                                    </figure>
+                                </a>
+                                <button type="button" class="add-to-cart-mt">
+                                    <i class="fa fa-shopping-cart"></i>
+                                    <span> Add to Cart</span>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div class="item-info">
+                            <div class="info-inner">
+                                <div class="item-title">
+                                    <a title="iPhone 8 Plus 256GB" href="iphone-8-plus-256gb-3.html">iPhone 8 Plus 256GB</a>
+                                </div>
+                                <div class="item-content">
+
+                                    <div class="item-price">
+                                        <div class="price-box">
+                                            <p class="special-price">
+                                                <span class="price"> 28,790,000 vnd</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+                <div class="product-item">
+                    <div class="item-inner">
+                        <div class="product-thumbnail">
+                            <div class="pr-img-area" style="height:300px">
+                                <a title="iPhone 8 256GB" href="iphone-8-256gb-4.html">
+                                    <figure>
+                                        <img class="first-img" src="public/source/images/products/iphone-8-256gb.png" alt="html template">
+                                        <img class="hover-img" src="public/source/images/products/iphone-8-256gb.png" alt="html template">
+                                    </figure>
+                                </a>
+                                <button type="button" class="add-to-cart-mt">
+                                    <i class="fa fa-shopping-cart"></i>
+                                    <span> Add to Cart</span>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div class="item-info">
+                            <div class="info-inner">
+                                <div class="item-title">
+                                    <a title="iPhone 8 256GB" href="iphone-8-256gb-4.html">iPhone 8 256GB</a>
+                                </div>
+                                <div class="item-content">
+
+                                    <div class="item-price">
+                                        <div class="price-box">
+                                            <p class="special-price">
+                                                <span class="price"> 25,790,000 vnd</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+                <div class="product-item">
+                    <div class="item-inner">
+                        <div class="product-thumbnail">
+                            <div class="pr-img-area" style="height:300px">
+                                <a title="iPhone 8 Plus 64GB" href="iphone-8-plus-64gb-5.html">
+                                    <figure>
+                                        <img class="first-img" src="public/source/images/products/iphone-8-plus-64gb.png" alt="html template">
+                                        <img class="hover-img" src="public/source/images/products/iphone-8-plus-64gb.png" alt="html template">
+                                    </figure>
+                                </a>
+                                <button type="button" class="add-to-cart-mt">
+                                    <i class="fa fa-shopping-cart"></i>
+                                    <span> Add to Cart</span>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div class="item-info">
+                            <div class="info-inner">
+                                <div class="item-title">
+                                    <a title="iPhone 8 Plus 64GB" href="iphone-8-plus-64gb-5.html">iPhone 8 Plus 64GB</a>
+                                </div>
+                                <div class="item-content">
+
+                                    <div class="item-price">
+                                        <div class="price-box">
+                                            <p class="special-price">
+                                                <span class="price"> 23,990,000 vnd</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="item col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+                <div class="product-item">
+                    <div class="item-inner">
+                        <div class="product-thumbnail">
+                            <div class="pr-img-area" style="height:300px">
+                                <a title="iPhone 8 64GB" href="iphone-8-64gb-6.html">
+                                    <figure>
+                                        <img class="first-img" src="public/source/images/products/iphone-8-64gb.png" alt="html template">
+                                        <img class="hover-img" src="public/source/images/products/iphone-8-64gb.png" alt="html template">
+                                    </figure>
+                                </a>
+                                <button type="button" class="add-to-cart-mt">
+                                    <i class="fa fa-shopping-cart"></i>
+                                    <span> Add to Cart</span>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div class="item-info">
+                            <div class="info-inner">
+                                <div class="item-title">
+                                    <a title="iPhone 8 64GB" href="iphone-8-64gb-6.html">iPhone 8 64GB</a>
+                                </div>
+                                <div class="item-content">
+
+                                    <div class="item-price">
+                                        <div class="price-box">
+                                            <p class="special-price">
+                                                <span class="price"> 20,990,000 vnd</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div
